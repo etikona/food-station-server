7bc6ad6b8f0d4d7bcce1447e61ac0c2a65f587e7
eti kona <etikonapal@gmail.com>
2024-10-22 00:57:51 +0600

    change the origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://food-station-client.vercel.app"],
+    origin: "*",
     credentials: true,
   })
 );
